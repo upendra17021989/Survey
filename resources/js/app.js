7513b4be805e952af8f50f337addadd9ecaddcc5
Upendrapratap Singh <upendraprataps@cybage.com>
2018-09-21 19:22:33 +0530

    Initial Commit

---
+++ resources/js/app.js
@@ -0,0 +1,23 @@
+require('./bootstrap');
+import React from 'react';
+import { render } from 'react-dom';
+import { Router, Route, browserHistory } from 'react-router';
+
+
+import Master from './components/Master';
+import CreateProduct from './components/CreateProduct';
+import DisplayProduct from './components/DisplayProduct';
+import DisplaySurvey from './components/DisplaySurvey';
+import UpdateProduct from './components/UpdateProduct';
+
+
+render(
+  <Router history={browserHistory}>
+      <Route path="/" component={Master} >
+        <Route path="/add-item" component={CreateProduct} />
+        <Route path="/display-item" component={DisplayProduct} />
+        <Route path="/display-survey" component={DisplaySurvey} />
+        <Route path="/edit/:id" component={UpdateProduct} />
+      </Route>
+    </Router>,
+        document.getElementById('crud-app'));
